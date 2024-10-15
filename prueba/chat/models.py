0ea6cd77f4594d49664c9c85f16f357b41a52ec7
Sherklan12e <97853791+Sherklan12e@users.noreply.github.com>
2024-10-15 17:45:44 -0300

    Changes admin

--- prueba/chat/models.py
+++ prueba/chat/models.py
@@ -9,4 +9,4 @@ class ChatMessage(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-    ordering = ['timestamp']
+        ordering = ['timestamp']
--- prueba/puplicaciones/admin.py
+++ prueba/puplicaciones/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Alquiler, ImagenAlquiler
 
-# Register your models here.
+admin.site.register(Alquiler)
+
+admin.site.register(ImagenAlquiler)
