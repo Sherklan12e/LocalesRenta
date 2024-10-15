0bad65ffa64e9f3088077e21e40c8858934ab291
Sherklan <97853791+Sherklan12e@users.noreply.github.com>
2024-10-15 00:17:47 -0300

    Avisar si inicias session desde otro dispositivo

--- prueba/users/signals.py
+++ prueba/users/signals.py
@@ -1,9 +1,13 @@
-# from django.db.models.signals import post_save
-# from django.dispatch import receiver
-# from django.contrib.auth.models import User
-# from .models import Profile
+from django.contrib.auth.signals import user_logged_in
+from django.dispatch import receiver
+from django.core.mail import send_mail
+from django.conf import settings
 
-# @receiver(post_save, sender=User)
-# def create_user_profile(sender, instance, created, **kwargs):
-#     if created:
-#         Profile.objects.create( username=instance.username)
+@receiver(user_logged_in)
+def send_login_notification(sender, request, user, **kwargs):
+    subject = 'Iniciaste sesión en otro dispositivo 😁😁😁'
+    message = f'Hola {user.username},\n\nHas iniciado sesión en tu cuenta desde un nuevo dispositivo.'
+    from_email = settings.EMAIL_HOST_USER
+    recipient_list = [user.email]
+
+    send_mail(subject, message, from_email, recipient_list)
