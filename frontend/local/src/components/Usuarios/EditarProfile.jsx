4fde415c4b639c1fad3b36edde304c71af00da77
Dario <dari.cortz345@gmail.com>
2024-10-21 17:46:20 -0300

    changes

--- frontend/local/src/components/Usuarios/EditarProfile.jsx
+++ frontend/local/src/components/Usuarios/EditarProfile.jsx
@@ -1,16 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import { motion } from 'framer-motion';
+import { motion ,AnimatePresence } from 'framer-motion';
 import { FaUser, FaMapMarkerAlt, FaFacebook, FaInstagram, FaPhone } from 'react-icons/fa';
 import { MdDescription } from 'react-icons/md';
+import { useDarkMode } from "../../contexts/DarkModeContext";
 
 function EditarProfile() {
+    const { darkMode } = useDarkMode();
     const [errors, setErrors] = useState({});
     const navigate = useNavigate();
     const [showNotification, setShowNotification] = useState(false);
     const token = localStorage.getItem('access_token');
     const { username } = useParams();
+    const [isModalOpen, setIsModalOpen] = useState(false);
     const [profileData, setProfileData] = useState({
         username: '',
         bio: '',
@@ -86,7 +89,7 @@ function EditarProfile() {
             setShowNotification(true);
             setTimeout(() => {
                 setShowNotification(false);
-                window.location.reload(); 
+                window.location.reload();
                 navigate(`/profile/${username}`);
             }, 500);
             console.log('Profile updated:', response.data);
@@ -94,12 +97,12 @@ function EditarProfile() {
             if (newUsername !== username) {
                 localStorage.setItem('username', newUsername);
                 navigate(`/profile/${newUsername}`);
-                window.location.reload(); 
+                window.location.reload();
             } else {
                 navigate(`/profile/${username}`);
-                window.location.reload(); 
+                window.location.reload();
             }
-            
+
         } catch (error) {
             console.error('Error updating profile:', error);
             if (error.response) {
@@ -115,9 +118,10 @@ function EditarProfile() {
             }
         }
     };
-
+    const openModal = () => setIsModalOpen(true);
+    const closeModal = () => setIsModalOpen(false);
     return (
-        <div className="min-h-screen bg-gradient-to-br from-indigo-600 to-purple-500 flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+        <div className={`min-h-screen ${darkMode ? 'bg-color_turquesa9 text-white ' : 'bg-gray-200  text-black'}  flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8`}>
             {showNotification && (
                 <motion.div
                     initial={{ opacity: 0, scale: 0.8 }}
@@ -129,24 +133,26 @@ function EditarProfile() {
                 </motion.div>
             )}
 
-            <div className="max-w-lg w-full bg-white rounded-3xl shadow-lg overflow-hidden">
+            <div className="max-w-lg w-full  rounded-3xl shadow-lg overflow-hidden">
                 <div className="md:flex">
-                    <div className="md:w-1/3 bg-gradient-to-br from-indigo-600 to-purple-500 p-6 flex flex-col items-center">
+                    <div className={`md:w-1/3   ${darkMode ? 'bg-color_turquesa9 text-white ' : 'bg-gray-100  text-black'}  p-6 flex flex-col items-center`}>
                         <motion.div
                             whileHover={{ scale: 1.1 }}
                             className="w-32 h-32 mb-4"
                         >
                             <img
-                                className="w-full h-full rounded-full border-4 border-gray-100"
+                                className="w-full h-full rounded-full border-4 border-gray-100  rounded-full object-cover shadow-lg transition-transform transform hover:scale-110"
                                 src={previewImage || "https://via.placeholder.com/150"}
                                 alt="Profile"
+                                onClick={openModal}
                             />
                         </motion.div>
-                        <h2 className="text-white text-lg font-semibold">Actualizar Foto</h2>
+                        <h2 className="
+                        text-lg font-semibold">Actualizar Foto</h2>
                     </div>
 
                     <div className="p-6 flex-grow">
-                        <h3 className="text-2xl font-semibold text-gray-700 text-center mb-6">Editar Perfil</h3>
+                        <h3 className="text-2xl font-semibold  text-center mb-6">Editar Perfil</h3>
                         <form onSubmit={handleSubmit} className="space-y-4">
                             {errors.general && <p className="text-red-500 text-center">{errors.general}</p>}
 
@@ -158,69 +164,69 @@ function EditarProfile() {
                                     value={profileData.username}
                                     onChange={handleChange}
                                     placeholder="Username"
-                                    className={`w-full py-2 px-4 rounded-lg shadow-sm focus:ring-indigo-500 border ${errors.username ? 'border-red-500' : 'border-gray-300'}`}
+                                    className={`w-full py-2 px-4 ${darkMode ? 'bg-slate-900 text-white ' : 'bg-white text-black'} rounded-lg shadow-sm focus:ring-indigo-500 border ${errors.username ? 'border-red-500' : 'border-gray-300'}`}
                                 />
                             </motion.div>
                             {errors.username && <p className="text-red-500 text-sm mt-1">{errors.username}</p>}
 
                             <motion.div whileHover={{ scale: 1.03 }} className="flex items-start space-x-2">
-                                <MdDescription className="text-indigo-500 mt-3" />
+                                <MdDescription className="text-gray-500 mt-3" />
                                 <textarea
                                     name="bio"
                                     value={profileData.bio ?? ''}
                                     onChange={handleChange}
                                     placeholder="Bio"
-                                    className={`w-full py-2 px-4 rounded-lg shadow-sm focus:ring-indigo-500 border ${errors.bio ? 'border-red-500' : 'border-gray-300'}`}
+                                    className={`w-full ${darkMode ? 'bg-slate-900 text-white ' : 'bg-white text-black'} py-2 px-4 rounded-lg shadow-sm focus:ring-indigo-500 border ${errors.bio ? 'border-red-500' : 'border-gray-300'}`}
                                     rows="3"
                                 />
                             </motion.div>
                             {errors.bio && <p className="text-red-500 text-sm mt-1">{errors.bio}</p>}
 
                             <motion.div whileHover={{ scale: 1.03 }} className="flex items-center space-x-2">
-                                <FaMapMarkerAlt className="text-indigo-500" />
+                                <FaMapMarkerAlt className="text-blue-500" />
                                 <input
                                     type="text"
                                     name="location"
                                     value={profileData.location}
                                     onChange={handleChange}
                                     placeholder="Location"
-                                    className="w-full py-2 px-4 rounded-lg shadow-sm focus:ring-indigo-500 border-gray-300"
+                                    className={`w-full py-2 px-4 text-black  ${darkMode ? 'bg-slate-900 text-white ' : 'bg-white text-black'}  rounded-lg shadow-sm focus:ring-indigo-500 border-gray-300`}
                                 />
                             </motion.div>
 
                             <motion.div whileHover={{ scale: 1.03 }} className="flex items-center space-x-2">
-                                <FaPhone className="text-indigo-500" />
+                                <FaPhone className="text-green-500" />
                                 <input
                                     type="text"
                                     name="phone_number"
                                     value={profileData.phone_number}
                                     onChange={handleChange}
                                     placeholder="Número de Teléfono"
-                                    className="w-full py-2 px-4 rounded-lg shadow-sm focus:ring-indigo-500 border-gray-300"
+                                    className={`${darkMode ? 'bg-slate-900 text-white ' : 'bg-white text-black'}  w-full py-2 px-4 rounded-lg shadow-sm focus:ring-indigo-500 `}
                                 />
                             </motion.div>
 
                             <motion.div whileHover={{ scale: 1.03 }} className="flex items-center space-x-2">
-                                <FaFacebook className="text-indigo-500" />
+                                <FaFacebook className="text-blue-400" />
                                 <input
                                     type="url"
                                     name="facebook"
                                     value={profileData.facebook}
                                     onChange={handleChange}
                                     placeholder="https://facebook.com/usuario"
-                                    className="w-full py-2 px-4 rounded-lg shadow-sm focus:ring-indigo-500 border-gray-300"
+                                    className={` ${darkMode ? 'bg-slate-900 text-white ' : 'bg-white text-black'}  w-full py-2 px-4 rounded-lg shadow-sm focus:ring-indigo-500 border-gray-300`}
                                 />
                             </motion.div>
 
                             <motion.div whileHover={{ scale: 1.03 }} className="flex items-center space-x-2">
-                                <FaInstagram className="text-indigo-500" />
+                                <FaInstagram className="text-pink-500" />
                                 <input
                                     type="url"
                                     name="instagram"
                                     value={profileData.instagram}
                                     onChange={handleChange}
                                     placeholder="https://instagram.com/usuario"
-                                    className="w-full py-2 px-4 rounded-lg shadow-sm focus:ring-indigo-500 border-gray-300"
+                                    className={`w-full py-2 ${darkMode ? 'bg-slate-900 text-white ' : 'bg-white text-black'} px-4 rounded-lg shadow-sm focus:ring-indigo-500 border-gray-300`}
                                 />
                             </motion.div>
 
@@ -229,18 +235,51 @@ function EditarProfile() {
                                     type="file"
                                     name="profile_picture"
                                     onChange={handleImageChange}
-                                    className="w-full text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:bg-indigo-100 file:text-indigo-700 hover:file:bg-indigo-200"
+                                    className={`w-full text-sm text-gray-500  ${darkMode ? 'bg-slate-900 text-white ' : 'bg-white text-black'} file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:bg-indigo-100 file:text-indigo-700 hover:file:bg-indigo-200`}
                                 />
                             </motion.div>
 
                             <motion.button
                                 whileHover={{ scale: 1.1 }}
                                 type="submit"
-                                className="w-full text-center py-3 px-6 bg-indigo-600 text-white font-bold rounded-lg shadow-lg transition duration-300 hover:bg-indigo-700"
+                                className={`w-full ${darkMode ? 'bg-slate-900 text-white ' : 'bg-white text-black'} text-center py-3 px-6 bg-indigo-600 font-bold rounded-lg shadow-lg transition duration-300 hover:bg-indigo-700`}
                             >
                                 Guardar Cambios
                             </motion.button>
                         </form>
+                        <AnimatePresence>
+                            {isModalOpen && (
+                                <motion.div
+                                    initial={{ opacity: 0 }}
+                                    animate={{ opacity: 1 }}
+                                    exit={{ opacity: 0 }}
+                                    className="fixed inset-0 bg-black bg-opacity-75 flex justify-center items-center z-50"
+                                    onClick={closeModal}
+                                >
+                                    <motion.div
+                                        initial={{ scale: 0.9 }}
+                                        animate={{ scale: 1 }}
+                                        exit={{ scale: 0.9 }}
+                                        className="relative max-w-4xl max-h-[90vh] overflow-hidden"
+                                        onClick={(e) => e.stopPropagation()}
+                                    >
+                                        <img
+                                            className="w-full h-full object-contain"
+                                            src={previewImage || "https://via.placeholder.com/1200x800"}
+                                            alt={previewImage}
+                                        />
+                                        <button
+                                            className="absolute top-4 right-4 text-white bg-black bg-opacity-50 rounded-full p-2 hover:bg-opacity-75 transition-all duration-300"
+                                            onClick={closeModal}
+                                        >
+                                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+                                            </svg>
+                                        </button>
+                                    </motion.div>
+                                </motion.div>
+                            )}
+                        </AnimatePresence>
                     </div>
                 </div>
             </div>
--- frontend/local/src/components/Usuarios/Profile.jsx
+++ frontend/local/src/components/Usuarios/Profile.jsx
@@ -3,11 +3,13 @@ import { RiInstagramFill, RiFacebookCircleFill } from "react-icons/ri";
 import { useParams, useNavigate, Link } from 'react-router-dom';
 import axios from 'axios';
 import { useDarkMode } from '../../contexts/DarkModeContext';
+import { motion, AnimatePresence } from 'framer-motion';
 
 function Profile() {
     const { username } = useParams();
     const [profile, setProfile] = useState(null);
     const [alquileres, setAlquileres] = useState([]);
+    const [isModalOpen, setIsModalOpen] = useState(false);
     const navigate = useNavigate();
     const token = localStorage.getItem('access_token');
     const currentUser = localStorage.getItem('username');
@@ -32,7 +34,7 @@ function Profile() {
     }, [username, token]);
 
     useEffect(() => {
-        if (!profile) return; 
+        if (!profile) return;
         const fetchAlquileres = async () => {
             try {
                 const response = await axios.get('http://127.0.0.1:8000/alquiler/alquileres/view/', {
@@ -40,7 +42,7 @@ function Profile() {
                         'Authorization': `Bearer ${token}`
                     }
                 });
-                
+
                 const userAlquileres = response.data.filter(alquiler => alquiler.user === profile.user);
                 setAlquileres(userAlquileres);
             } catch (error) {
@@ -57,7 +59,8 @@ function Profile() {
     if (!profile) {
         return <div className="flex justify-center items-center min-h-screen bg-gray-100">Loading...</div>;
     }
-
+    const openModal = () => setIsModalOpen(true);
+    const closeModal = () => setIsModalOpen(false);
     return (
         <div className={`min-h-screen  ${darkMode ? 'bg-color_turquesa9 text-white ' : 'bg-white  text-black'}  flex justify-center py-12 px-4 lg:px-8`} >
             <div className={`${darkMode ? 'bg-color_turquesa10 text-white ' : 'bg-white  text-black'} shadow-2xl rounded-3xl p-10 w-full max-w-6xl   `}>
@@ -66,18 +69,30 @@ function Profile() {
                         className="w-36 h-36 rounded-full object-cover shadow-lg transition-transform transform hover:scale-110"
                         src={profile.profile_picture}
                         alt="Profile"
+                        onClick={openModal}
                     />
                     <div className="flex-grow">
                         <h1 className="text-4xl font-bold  tracking-wider">{profile.username}</h1>
                         <p className="text-lg  mt-2">@{profile.username}</p>
                         <p className=" mt-1 text-sm">{profile.location}</p>
                         <div className="mt-4 flex items-center space-x-4">
-                            <a href={profile.instagram} target='_blank' rel="noopener noreferrer" className="text-red-500 hover:text-red-600 transition duration-300">
-                                <RiInstagramFill className='text-4xl' />
-                            </a>
-                            <a href={profile.facebook} target='_blank' rel="noopener noreferrer" className="text-blue-500 hover:text-blue-600 transition duration-300">
-                                <RiFacebookCircleFill className='text-4xl' />
-                            </a>
+                            {
+                                profile.instagram ?
+                                    <a href={profile.instagram} className="text-red-500 hover:text-red-600 transition duration-300">
+                                        <RiInstagramFill className='text-4xl' />
+                                    </a>
+                                    : null
+                            }
+
+
+                            {
+                                profile.facebook ?
+                                    <a href={profile.facebook} target='_blank' rel="noopener noreferrer" className="text-blue-500 hover:text-blue-600 transition duration-300">
+                                        <RiFacebookCircleFill className='text-4xl' />
+                                    </a>
+                                    : null
+                            }
+
                         </div>
                     </div>
                 </div>
@@ -87,23 +102,23 @@ function Profile() {
                 </div>
 
                 <div className="flex justify-between items-center mt-8">
-                {currentUser === profile.username ? (
+                    {currentUser === profile.username ? (
                         <Link to='edit' className="inline-block text-blue-600 font-semibold underline transition duration-300 hover:text-blue-800">
                             Editar Perfil
                         </Link>
                     ) : null}
 
-                        <Link to={`/chat/${profile.username}`} className="bg-gradient-to-r from-blue-500 to-blue-700 text-white py-2 px-6 rounded-full hover:shadow-xl transform transition-transform hover:-translate-y-1 hover:bg-gradient-to-r hover:from-blue-600 hover:to-blue-800">
-                            Enviar mensaje
-                        </Link>
+                    <Link to={`/chat/${profile.username}`} className="bg-gradient-to-r from-blue-500 to-blue-700 text-white py-2 px-6 rounded-full hover:shadow-xl transform transition-transform hover:-translate-y-1 hover:bg-gradient-to-r hover:from-blue-600 hover:to-blue-800">
+                        Enviar mensaje
+                    </Link>
                 </div>
-                  
-                    
+
+
                 <div className="mt-12">
                     <h2 className="text-2xl font-bold mb-4">Posts</h2>
                     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
                         {alquileres.map(alquiler => (
-                            
+
                             profile.user === alquiler.user && (
                                 <div key={alquiler.id} className="relative group rounded-lg overflow-hidden shadow-lg transform transition-transform hover:scale-105">
                                     {alquiler.imagenes && alquiler.imagenes.length > 0 ? (
@@ -125,6 +140,39 @@ function Profile() {
                         ))}
                     </div>
                 </div>
+                <AnimatePresence>
+                    {isModalOpen && (
+                        <motion.div
+                            initial={{ opacity: 0 }}
+                            animate={{ opacity: 1 }}
+                            exit={{ opacity: 0 }}
+                            className="fixed inset-0 bg-black bg-opacity-75 flex justify-center items-center z-50"
+                            onClick={closeModal}
+                        >
+                            <motion.div
+                                initial={{ scale: 0.9 }}
+                                animate={{ scale: 1 }}
+                                exit={{ scale: 0.9 }}
+                                className="relative max-w-4xl max-h-[90vh] overflow-hidden"
+                                onClick={(e) => e.stopPropagation()}
+                            >
+                                <img
+                                    className="w-full h-full object-contain"
+                                    src={profile.profile_picture || "https://via.placeholder.com/1200x800"}
+                                    alt={profile.username}
+                                />
+                                <button
+                                    className="absolute top-4 right-4 text-white bg-black bg-opacity-50 rounded-full p-2 hover:bg-opacity-75 transition-all duration-300"
+                                    onClick={closeModal}
+                                >
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+                                    </svg>
+                                </button>
+                            </motion.div>
+                        </motion.div>
+                    )}
+                </AnimatePresence>
             </div>
         </div>
     );
